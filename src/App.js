f1db304a2b1e57e2b4d7f5e370d1b1d1f8df6b14
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2022-01-25 14:37:36 +0100

    Finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState,useEffect} from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import {getPlacesData} from './api'
 
-function App() {
+
+import Header from './components/Header/Header';
+import List from './components/List/List'
+import Map from './components/Map/Map'
+
+const App =()=> {
+  const [places, setPlaces] = useState([])
+  const [coordinates,setCoordinates]= useState({})
+  const [bounds, setBounds]=useState({})
+  const [type, setType] = useState("restaurants");
+  const [raiting, setRaiting] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(()=>{
+      navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
+            setCoordinates({lat:latitude,lng:longitude})
+      })
+  },[])
+
+  useEffect(()=>{
+    
+    setIsLoading(true);
+    getPlacesData(type, bounds.sw , bounds.ne)
+    .then((data)=>{
+     
+      setPlaces(data?.filter((place)=>place.name > 0))
+      setFilteredPlaces([])
+      setIsLoading(false)
+    })
+  
+
+  },[type,coordinates,bounds])
+
+useEffect(() => {
+  const filteredPlaces = places.filter((place)=> place.rating > raiting)
+    setFilteredPlaces(filteredPlaces)
+}, [raiting]);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  <>
+
+  <CssBaseline/>
+
+  <Header setCoordinates={setCoordinates}/>
+  <Grid container spacing={3} style={{width:'100%'}}>
+
+    <Grid item xs={12} md={4}>
+      <List places = {filteredPlaces.length ? filteredPlaces : places} 
+      childClicked={childClicked}
+      isLoading={isLoading}
+      type={type}
+      setType={setType}
+      raiting={raiting}
+      setRaiting={setRaiting}
+
+      
+      />
+    </Grid>
+    <Grid item xs={12} md={8}>
+    <Map  setCoordinates={setCoordinates} 
+    setBounds={setBounds}
+    coordinates ={coordinates}
+    places={filteredPlaces.length ? filteredPlaces : places} 
+    setChildClicked={setChildClicked}
+    />
+    </Grid>
+  </Grid>
+
+   
+  </>
+  
   );
 }
 
-export default App;
+export default App
